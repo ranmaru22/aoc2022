736c0b29d658442799c5697ae5a91e4443cf1333
Alex Sun <alexsun82@icloud.com>
2022-12-06 22:58:42 +0100

    Add day 2

--- src/day2.lisp
+++ src/day2.lisp
@@ -13,4 +13,43 @@
             (self (read-from-string (subseq line 2))))
         (push (cons opponent self) ret)))))
 
-;; TODO
+(defun score-move (move)
+  (destructuring-bind (opponent . self) move
+    (case opponent
+      (a (case self
+           (x (+ 3 1))
+           (y (+ 6 2))
+           (z (+ 0 3))))
+      (b (case self
+           (x (+ 0 1))
+           (y (+ 3 2))
+           (z (+ 6 3))))
+      (c (case self
+           (x (+ 6 1))
+           (y (+ 0 2))
+           (z (+ 3 3)))))))
+
+(defun calculate-move (move)
+  (destructuring-bind (opponent . outcome) move
+    (case outcome
+      ;; Lose
+      (x (case opponent
+           (a (+ 0 3))
+           (b (+ 0 1))
+           (c (+ 0 2))))
+      ;; Draw
+      (y (case opponent
+           (a (+ 3 1))
+           (b (+ 3 2))
+           (c (+ 3 3))))
+      ;; Win
+      (z (case opponent
+           (a (+ 6 2))
+           (b (+ 6 3))
+           (c (+ 6 1)))))))
+
+(defun part-1 ()
+  (reduce #'+ (mapcar #'score-move (get-strategy))))
+
+(defun part-2 ()
+  (reduce #'+ (mapcar #'calculate-move (get-strategy))))
